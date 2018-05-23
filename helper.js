cd0aeb1d32b7e472cdf5d33676537faff0dd9365
Odselmaa <se11d002@gmail.com>
2018-05-24 01:51:18 +0300

    yep

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 module.exports = {
     send_request: function (options, callback) {
-        
+        options.timeout = 20000
         request(options, function (error, response, body) {
             callback(error, response, body)
         });
