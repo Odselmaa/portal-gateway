3b7cdf6c7ee8db634049196094b8b3f5f46851da
Odselmaa <se11d002@gmail.com>
2018-05-24 01:57:52 +0300

    yep

--- app.js
+++ app.js
@@ -112,6 +112,8 @@ if (cluster.isMaster) {
     var server = http.listen(PORT, () => {
         console.log("server is listening on port", server.address().port)
     })
+    server.setTimeout(5000000);
+
     // }
     module.exports = app; // for testing
 
