4e766e43e36b62e77e74c6edf799f4836db519c7
Odselmaa <se11d002@gmail.com>
2018-05-24 01:18:00 +0300

    yep

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,9 +2,13 @@ var h = require('../helper.js')
 
 module.exports = {
     user_api: function (req, res) {
+        if(req.method!="GET")
+            payload = req.body
+        else
+            payload = {}
         var options = {
             uri: USER_API_ROOT + req.url,
-            json: req.body,
+            json: payload,
             method: req.method,
             // headers: req.headers,
             // rejectUnauthorized: false
