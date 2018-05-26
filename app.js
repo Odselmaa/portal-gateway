8f0fa9103fec3a6a4370fe577da228ff44204ba8
Odselmaa <se11d002@gmail.com>
2018-05-26 03:10:57 +0300

    added news url

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ if (cluster.isMaster) {
     CHAT_API_ROOT = 'https://portal-chat.herokuapp.com' 
                     //"http://localhost:5002"
     AUTH_API_ROOT = USER_API_ROOT
-    NEWS_API_ROOT = 'http://127.0.0.1:5003'
+    NEWS_API_ROOT = 'https://portal-news-api.herokuapp.com'
     REVIEW_API_ROOT = 'https://portal-review.herokuapp.com'
     const userServiceProxy = httpProxy(USER_API_ROOT)
 
