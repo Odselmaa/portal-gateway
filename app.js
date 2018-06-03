1840be7f82cd7c4e3889dbccfa0171cb0adb8d63
Odselmaa <se11d002@gmail.com>
2018-06-04 01:09:54 +0300

    api fixing

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ if (cluster.isMaster) {
 
     app.post('/api/auth', userServiceProxy)
     app.get('/api/user', [m.authMiddleware], httpProxy(getUserUrl))
-    app.post('/api/user', [m.authMiddleware], httpProxy(getUserUrl))
+    app.post('/api/user',  httpProxy(getUserUrl))
     app.get('/api/user/:user_id',[m.authMiddleware],  httpProxy(getUserUrl))
     app.put('/api/user/:user_id', [m.authMiddleware], httpProxy(getUserUrl))
     app.get('/api/user/:user_id/friend', [m.authMiddleware], httpProxy(getUserUrl))
