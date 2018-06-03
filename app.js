686048d2ee1921eaf9f15dd78a72e93a2acd892d
Odselmaa <se11d002@gmail.com>
2018-06-03 21:27:10 +0300

    lb 2

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ if (cluster.isMaster) {
     REVIEW_API_ROOT = 'https://portal-review.herokuapp.com'
 
     let cur = 0
-    let servers = [USER_API_ROOT, "https://portal-user-app.herokuapp.com", "https://portal-user1.herokuapp.com"]
+    let servers = [USER_API_ROOT, "https://portal-user-app.herokuapp.com", "https://portal-user1.herokuapp.com","https://portal-user2.herokuapp.com" ]
     const userServiceProxy = httpProxy(USER_API_ROOT)
     const reportServiceProxy = httpProxy(REPORT_API_ROOT)
     const chatServiceProxy = httpProxy(CHAT_API_ROOT)
