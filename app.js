e8d2ea39cee974e41f43dfbbe1b30752d5539044
Odselmaa <se11d002@gmail.com>
2018-06-20 04:49:23 +0300

    server urls

--- app.js
+++ app.js
@@ -40,10 +40,12 @@ if (cluster.isMaster) {
 
 
     let cur = 0
-    let servers = [urls.USER_API_ROOT
-        // "https://portal-user-app.herokuapp.com",
+    let servers = 
+    [
+        // urls.USER_API_ROOT,
+        "https://portal-user-app.herokuapp.com"
         // "https://portal-user1.herokuapp.com",
-        // "https://portal-user2.herokuapp.com"
+        // "https://portal-user2.herokuapp.com",
         // "https://portal-user3.herokuapp.com",
         // "https://portal-user5.herokuapp.com",
         // "https://portal-user7.herokuapp.com",
