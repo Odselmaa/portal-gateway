242354adbc4747f6be449b2ad348100c3eef9306
Odselmaa <se11d002@gmail.com>
2018-06-25 11:06:22 +0300

    url testing

--- app.js
+++ app.js
@@ -42,19 +42,19 @@ if (cluster.isMaster) {
 
     let cur = 0
     let servers = [
-        urls.USER_API_ROOT
-        // "https://portal-user-app.herokuapp.com",
-        // "https://portal-user1.herokuapp.com",
-        // "https://portal-user2.herokuapp.com"
-        // "https://portal-user3.herokuapp.com",
-        // "https://portal-user4.herokuapp.com",
-        // "https://portal-user5.herokuapp.com",
-        // "https://portal-user6.herokuapp.com",
-        // "https://portal-user7.herokuapp.com",
-        // "https://portal-user8.herokuapp.com",
-        // "https://portal-user-9.herokuapp.com",
-        // "https://portal-user10.herokuapp.com",
-        // "https://portal-user11.herokuapp.com"
+        urls.USER_API_ROOT,
+        "https://portal-user-app.herokuapp.com",
+        "https://portal-user1.herokuapp.com",
+        "https://portal-user2.herokuapp.com",
+        "https://portal-user3.herokuapp.com",
+        "https://portal-user4.herokuapp.com",
+        "https://portal-user5.herokuapp.com",
+        "https://portal-user6.herokuapp.com",
+        "https://portal-user7.herokuapp.com",
+        "https://portal-user8.herokuapp.com",
+        "https://portal-user-9.herokuapp.com",
+        "https://portal-user10.herokuapp.com",
+        "https://portal-user11.herokuapp.com"
 
     ]
 
